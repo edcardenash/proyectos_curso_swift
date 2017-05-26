472226fd872d4c1bf016e46e5bef62823f46e24a
edcardenash <ercardenas@uc.cl>
2017-05-26 14:42:52 -0400

    Juego de memoria.playground

---
+++ Juego de memoria.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Iteración del rango
+for rango in 0...100 {
+    
+    //Números divisibles entre 5
+    if (rango % 5 == 0) {
+        print((rango), "Bingo!!!")
+    }
+    // Números pares
+    if (rango % 2 == 0){
+        print((rango), "par!!!")
+    }
+        // Números impares
+    else {
+        print((rango), "impar!!!")
+    };
+    // Números entre el 30 y 40
+    if (rango >= 30 && rango <= 40) {
+        print((rango), "Viva Swift!!!")
+    }
+}
